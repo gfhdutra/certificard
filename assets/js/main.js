e853c14164d70de3b9c787c4d4f102478cffe0f7
Gabriel Dutra <gfhdutra@gmail.com>
2021-10-03 17:12:53 -0300

    add portfolio

--- assets/js/main.js
+++ assets/js/main.js
@@ -60,26 +60,6 @@ skillsHeader.forEach(el => {
   el.addEventListener('click', toggleSkills)
 })
 
-/*==================== QUALIFICATION TABS ====================*/
-const tabs = document.querySelectorAll('[data-target]')
-const tabContents = document.querySelectorAll('[data-content]')
-
-tabs.forEach(tab => {
-  tab.addEventListener('click', () => {
-    const target = document.querySelector(tab.dataset.target)
-
-    tabContents.forEach(tabContent => {
-      tabContent.classList.remove('qualification__active')
-    })
-    target.classList.add('qualification__active')
-
-    tabs.forEach(tab => {
-      tab.classList.remove('qualification__active')
-    })
-    tab.classList.add('qualification__active')
-  })
-})
-
 /*==================== PORTFOLIO SWIPER  ====================*/
 let swiper = new Swiper('.portfolio__container', {
   cssMode: true,
@@ -114,6 +94,7 @@ function scrollActive() {
 }
 window.addEventListener('scroll', scrollActive)
 
+
 /*==================== CHANGE BACKGROUND HEADER ====================*/
 function scrollHeader() {
   const nav = document.getElementById('header')
@@ -126,6 +107,7 @@ function scrollHeader() {
   }
 }
 window.addEventListener('scroll', scrollHeader)
+
 
 /*==================== SHOW SCROLL UP ====================*/
 function scrollUp() {
@@ -140,32 +122,24 @@ function scrollUp() {
 }
 window.addEventListener('scroll', scrollUp)
 
-/*==================== DARK LIGHT THEME ====================*/
-const themeButton = document.getElementById('theme-button')
-const darkTheme = 'dark-theme'
-const iconTheme = 'uil-sun'
 
-// Previously selected topic (if user selected)
-const selectedTheme = localStorage.getItem('selected-theme')
-const selectedIcon = localStorage.getItem('selected-icon')
+/*==================== DARK/LIGHT THEME ====================*/
+const switchDarkMode = document.getElementById('theme-button')
+const currentTheme = localStorage.getItem("theme")
 
-// We obtain the current theme that the interface has by validating the dark-theme class
-const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dark' : 'light'
-const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'uil-moon' : 'uil-sun'
-
-// We validate if the user previously chose a topic
-if (selectedTheme) {
-  // If the validation is fulfilled, we ask what the issue was to know if we activated or deactivated the dark
-  document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme)
-  themeButton.classList[selectedIcon === 'uil-moon' ? 'add' : 'remove'](iconTheme)
+if (currentTheme == "dark") {
+    document.body.classList.add('dark-theme')
+    switchDarkMode.className = "uil uil-moon change-theme"
 }
 
-// Activate / deactivate the theme manually with the button
-themeButton.addEventListener('click', () => {
-    // Add or remove the dark / icon theme
-    document.body.classList.toggle(darkTheme)
-    themeButton.classList.toggle(iconTheme)
-    // We save the theme and the current icon that the user chose
-    localStorage.setItem('selected-theme', getCurrentTheme())
-    localStorage.setItem('selected-icon', getCurrentIcon())
+switchDarkMode.addEventListener("click", () => {
+    document.body.classList.toggle('dark-theme')
+
+    let theme = "light"
+    switchDarkMode.className = "uil uil-moon change-theme"
+    if (document.body.classList.contains('dark-theme')) {
+        theme = "dark"
+        switchDarkMode.className = "uil uil-sun change-theme"
+    }
+    localStorage.setItem("theme", theme)
 })
